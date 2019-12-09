faa2dace626dc5ab35af3bcadbcaf564457c2799
Michał Wdowski <michal.wdowski99@gmail.com>
2019-12-09 21:03:54 +0100

    problem z dostaniem się do komponentu Button

--- Assets/Scripts/UITest.cs
+++ Assets/Scripts/UITest.cs
@@ -1,55 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UITest : MonoBehaviour
 {
     // testValue - value that is going to change when AddTest() or SubstractTest() is called
-    public float testValue = 2137f;
+    [SerializeField] private float testValue = 2137f;
     // multiplier - value that is going to change when TestMultiplier() is called
-    public float multiplier = 1f;
+    [SerializeField] private float multiplier = 1f;
     // anotherMultiplier - value that is going to change when TestAnotherMultiplier() is called
-    public float anotherMultiplier = 1f;
+    [SerializeField] private float anotherMultiplier = 1f;
+    // addButton, substractButton, multiplyToggle, multiplySlider - actions on these controls will affect testValue
+    [SerializeField] private GameObject addButton;//, substractButton;
+    //[SerializeField] private GameObject multiplyToggle;
+    //[SerializeField] private GameObject multiplySlider;
 
-    // Start is called before the first frame update
-    void Start()
+    //
+    public void Start()
     {
-        
-    }
+        Button addButtonButton = addButton.GetComponent<Button>();
+        addButtonButton.OnClick.AddListener(TestAdd);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     // TestAdd adds 1 to testValue
-    public void TestAdd()
+    private void TestAdd()
     {
-        this.testValue += this.multiplier * this.anotherMultiplier;
+        testValue += multiplier * anotherMultiplier;
         return;
     }
 
     // TestAdd substracts 1 from testValue
-    public void TestSubstract()
+    private void TestSubstract()
     {
-        this.testValue -= this.multiplier * this.anotherMultiplier;
+        testValue -= multiplier * anotherMultiplier;
         return;
     }
 
     // TestMultiplier changes multiplier
-    public void TestMultiplier()
+    private void TestMultiplier()
     {
-        if (this.multiplier != 1f) this.multiplier = 1f;
-        else this.multiplier = 2f;
+        if (multiplier != 1f)
+        {
+            multiplier = 1f;
+        }
+        else
+        {
+            multiplier = 2f;
+        }
         return;
     }
 
     // TestAnotherMultiplier changes anotherMultiplier
-    public void TestAnotherMultiplier(float value)
+    private void TestAnotherMultiplier(float value)
     {
-        this.anotherMultiplier = value;
+        anotherMultiplier = value;
         return;
     }
-
 }
