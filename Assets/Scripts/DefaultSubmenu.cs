b4554f8beeae865f6cd257aed0d0aa268399c7a0
kanaji <pilifok2@gmail.com>
2019-12-13 22:18:49 +0100

    Problem z cofaniem do MainMenu

---
+++ Assets/Scripts/DefaultSubmenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefaultSubmenu : MonoBehaviour
+{
+    [SerializeField] private Button button;
+
+    public Button Button
+    {
+        get
+        {
+            return button;
+        }
+    }
+
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private DefaultSubmenu optionsMenu;
+    [SerializeField] private DefaultSubmenu creditsMenu;
+
+    private void Awake() //na starcie gry menu opcji i credits zostają wyłączone
+    {
+        optionsMenu.gameObject.SetActive(false);
+        creditsMenu.gameObject.SetActive(false);
+    }
+
+    private DefaultSubmenu currentSubmenu;
+
+    private void OnBackButtonClicked() //cofniecie sie do MainMenu
+    {
+        currentSubmenu = null;
+    }
+
+	private DefaultSubmenu CurrentSubmenu
+	{
+		get
+		{
+			return currentSubmenu;
+		}
+
+		set
+		{
+			if (currentSubmenu == null)
+			{
+				gameObject.SetActive(false);
+			}
+			else
+			{
+				currentSubmenu.gameObject.SetActive(false);
+				currentSubmenu.Button.onClick.RemoveListener(OnBackButtonClicked);
+			}
+
+			currentSubmenu = value;
+
+			if (currentSubmenu == null)
+			{
+				gameObject.SetActive(true);
+			}
+			else
+			{
+				currentSubmenu.gameObject.SetActive(true);
+				currentSubmenu.Button.onClick.AddListener(OnBackButtonClicked);
+			}
+		}
+	}
+
+	public void OpenOptionsMenu()
+    {
+       CurrentSubmenu = optionsMenu;
+    }
+
+    public void OpenCreditsMenu()
+    {
+       CurrentSubmenu = creditsMenu;
+    }
+
+}
