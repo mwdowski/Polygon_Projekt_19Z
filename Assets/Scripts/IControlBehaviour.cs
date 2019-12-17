ddd1206c31d1a5bdddac95f53c8e35f30be139c0
Michał Wdowski <michal.wdowski99@gmail.com>
2019-12-17 01:51:59 +0100

    Dodano interfejs IControlBehaviour

---
+++ Assets/Scripts/IControlBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public interface IControlBehaviour
+{
+    // Interface implemented by controls in OptionsMenu
+
+    // SetUpListener turns on a listener on control and delegates a method on target
+    void SetUpListener(GameObject target);
+}
