18ad5c6310b807aa286722dfc07050bbe8d6ef4e
kanaji <pilifok2@gmail.com>
2020-01-11 01:08:09 +0100

    Poruszanie za pomocą AddForce i collidery na granicach Canvas, i oddalenie kamery

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,20 +6,26 @@ public class Game : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
-    Vector2 movement;
-
     public Rigidbody2D square;
     public BoxCollider2D change_scene;
+    public Vector2 movement;
+    private Vector2 Bounds;
+
+    void Start()
+    {
+        //Bounds = Camera.main.ScreenToWorldPoint(new Vector 3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2f, 2f), Mathf.Clamp(transform.position.y, -4f, 4f), transform.position.z);
     }
 
     void FixedUpdate()
     {
-        square.MovePosition(square.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //square.MovePosition(square.position + movement * moveSpeed * Time.fixedDeltaTime);
+        square.AddForce(movement * moveSpeed);
     }
 
     void OnTriggerEnter2D(Collider2D col)
