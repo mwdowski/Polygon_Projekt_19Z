a30c21bbad32a1eef56125565378aca2275dfa23
Wooki <wooki1@op.pl>
2020-01-13 07:46:29 +0100

    Merge branch 'master' into Game_Scene
    # Conflicts:
    #	Assets/Prefabs/Menus/OptionsMenu.prefab
    #	Assets/Scenes/MainMenu.unity
    #	Assets/Scripts/Menus/MainMenu.cs

---
+++ Assets/Scripts/Menus/DefaultSubmenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+
+public class DefaultSubmenu : MonoBehaviour
+{
+    [SerializeField] protected Button backButton;
+
+
+    protected virtual void Awake()
+    {
+        Assert.IsNotNull(backButton);    
+    }
+
+    public Button Button
+    {
+        get
+        {
+            return backButton;
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+	[SerializeField] private DefaultSubmenu howToPlayMenuPrefab = null;
+	[SerializeField] private DefaultSubmenu optionsMenuPrefab = null;
+    [SerializeField] private DefaultSubmenu creditsMenuPrefab = null;
+	private DefaultSubmenu howToPlayMenuInstance = null;
+	private DefaultSubmenu optionsMenuInstance = null;
+	private DefaultSubmenu creditsMenuInstance = null;
+	private DefaultSubmenu currentSubmenu = null;
+
+
+	//Automatycznie przechodzi miedzy wybranymi menu i cofa do glownego menu.
+	private DefaultSubmenu CurrentSubmenu
+	{
+		get
+		{
+			return currentSubmenu;
+		}
+
+		set
+		{
+			if (currentSubmenu == null)
+			{
+				gameObject.SetActive(false);
+			}
+			else
+			{
+				currentSubmenu.gameObject.SetActive(false);
+				currentSubmenu.Button.onClick.RemoveListener(OnBackButtonClicked);
+			}
+			currentSubmenu = value;
+			if (currentSubmenu == null)
+			{
+				gameObject.SetActive(true);
+			}
+			else
+			{
+				currentSubmenu.gameObject.SetActive(true);
+				currentSubmenu.Button.onClick.AddListener(OnBackButtonClicked);
+			}
+		}
+	}
+
+
+	//Na starcie gry podległe menu zostają stworzone i wyłączone.
+	private void Awake()
+	{
+		Assert.IsNotNull(howToPlayMenuPrefab, "Missing howToPlayMenuPrefab on: " + gameObject.name);
+		Assert.IsNotNull(optionsMenuPrefab, "Missing optionsMenuPrefab on: " + gameObject.name);
+		Assert.IsNotNull(creditsMenuPrefab, "Missing creditsMenuPrefab on: " + gameObject.name);		
+		howToPlayMenuInstance = Instantiate(howToPlayMenuPrefab, transform.parent);
+		howToPlayMenuInstance.gameObject.SetActive(false);
+		optionsMenuInstance = Instantiate(optionsMenuPrefab, transform.parent);
+		optionsMenuInstance.gameObject.SetActive(false);
+		creditsMenuInstance = Instantiate(creditsMenuPrefab, transform.parent);
+		creditsMenuInstance.gameObject.SetActive(false);
+	}
+
+	//Cofniecie sie do MainMenu.
+	private void OnBackButtonClicked()
+	{
+		CurrentSubmenu = null;
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Game", LoadSceneMode.Single);
+	}
+
+	public void OpenHowToPlay()
+	{
+		CurrentSubmenu = howToPlayMenuInstance;
+	}
+
+	public void OpenOptionsMenu()
+	{
+		CurrentSubmenu = optionsMenuInstance;
+	}
+
+	public void OpenCreditsMenu()
+	{
+		CurrentSubmenu = creditsMenuInstance;
+	}
+
+	public void QuitGame()
+    {
+		Application.Quit();
+	}
+}
