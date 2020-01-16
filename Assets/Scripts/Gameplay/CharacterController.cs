ffb92ed5023f7c0e358bca1b31edabff3120c5db
kanaji <pilifok2@gmail.com>
2020-01-16 22:10:02 +0100

    Skakanie tylko od podłoża i postać nie zatrzymuje się w powietrzu przy napieraniu na ścianę

--- Assets/Scripts/Gameplay/CharacterController.cs
+++ Assets/Scripts/Gameplay/CharacterController.cs
@@ -7,7 +7,7 @@ public class CharacterController : MonoBehaviour
 	[SerializeField] private float moveSpeed = 5.0f;
 	[SerializeField] private float jumpForce = 5.0f;
 	private Rigidbody2D rigidbody;
-
+	public LayerMask Ground;
 
 	private void Awake()
 	{
@@ -17,10 +17,26 @@ public class CharacterController : MonoBehaviour
 
 	private void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
 		{
 			rigidbody.AddForce(new Vector2(0.0f, jumpForce));
 		}
 		rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, rigidbody.velocity.y);
 	}
+
+	bool IsGrounded()
+	{
+		Vector2 position = transform.position;
+		Vector2 direction = Vector2.down;
+		float distance = 1.0f;
+
+		RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, Ground);
+		if (hit.collider != null)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
 }
