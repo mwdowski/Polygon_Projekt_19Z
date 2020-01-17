65c6173c906c635c03adf6d2b2eef80b750b61fd
Michał Wdowski <michal.wdowski99@gmail.com>
2020-01-17 21:59:45 +0100

    Dodano pocisk Dodano strzelanie w kierunku, w którym jest wektor prędkości (jak gracz stoi, to pocisk leci w prawo) Dodano niszczenie pocisku na ścianach i wrogach

---
+++ Assets/Scripts/Gameplay/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D rigidbody;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string hitTag = collision.collider.gameObject.tag;
+        if (hitTag == "Enemy" || hitTag == "Walls")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Gameplay/CharacterController.cs
+++ Assets/Scripts/Gameplay/CharacterController.cs
@@ -10,20 +10,54 @@ public class CharacterController : MonoBehaviour
 	public LayerMask Ground;
 	private const float GROUNDED_RAYCAST_DISTANCE = 1.0f;
 
+	[SerializeField] private float bulletSpeed = 10000f;
+	[SerializeField] private GameObject bulletPrefab;
+	private static float halfWidth;
+	private GameObject bullet;
+
 
 	private void Awake()
 	{
 		rigidbody = GetComponent<Rigidbody2D>();
 		Assert.IsNotNull(rigidbody);
+
+		// znalezienie połowy szerokości - pomoże to przy tworzeniu pocisków
+		halfWidth = GetComponent<Renderer>().bounds.extents.x + bulletPrefab.GetComponent<Renderer>().bounds.extents.x;
 	}
 
 	private void Update()
 	{
+		// skok
 		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
 		{
 			rigidbody.AddForce(new Vector2(0.0f, jumpForce));
 		}
+
 		rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, rigidbody.velocity.y);
+
+		// strzał - strzelamy klawiszem "F"
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			if (rigidbody.velocity.x >= 0)
+			{
+				// idzie wprawo lub stoi w miejscu
+				bullet = Instantiate(bulletPrefab, new Vector3(transform.position.x + halfWidth, transform.position.y, transform.position.z), transform.rotation);
+				bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed, 0);
+			}
+			else
+			{
+				//idzie w lewo
+				bullet = Instantiate(bulletPrefab, new Vector3(transform.position.x - halfWidth, transform.position.y, transform.position.z), transform.rotation);
+
+				// ta część kodu obraca pocisk tak, by leciał w drugą stronę
+				Vector3 newScale = bullet.transform.localScale;
+				newScale *= -1;
+				bullet.transform.localScale = newScale;
+
+				// nadanie prędkości
+				bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(-bulletSpeed, 0);
+			}
+		}
 	}
 
 	private bool IsGrounded()
